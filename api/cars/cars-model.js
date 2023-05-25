7c12dddaab15a1fd85590e17b912951492928582
Egemen <sucuogluegemen1@gmail.com>
2023-05-25 03:18:19 +0300

    görav tamamlandı

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,28 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // HOKUS POKUS
-}
+  return db("cars");
+};
 
-const getById = () => {
-  // HOKUS POKUS
-}
+const getByVin = (vin) => {
+  return db("cars").where("vin", vin).first();
+};
 
-const create = () => {
+const getById = (id) => {
   // HOKUS POKUS
-}
+  return db("cars").where("id", id).first();
+};
+
+const create = async (cars) => {
+  // HOKUS POKUS
+  let [id] = await db("cars").insert(cars);
+  return getById(id);
+};
+
+module.exports = {
+  getAll,
+  getByVin,
+  getById,
+  create,
+};
